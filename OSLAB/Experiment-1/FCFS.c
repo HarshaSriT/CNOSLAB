fa7082193139c619febc5370c02734702783978a
HarshaSriT <94516270+HarshaSriT@users.noreply.github.com>
2022-09-06 11:58:25 +0530

    T.Harsha sri created FCFS.c

--- OSLAB/Experiment-1/FCFS.c
+++ OSLAB/Experiment-1/FCFS.c
@@ -1,6 +1,6 @@
 /* Name Of the Candidate: T.Harsha sri
    Roll No: 204G1A0536
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 30-08-2022
-   Date of Execution: 30-08-2022
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
 */
